2e91605e7ff67085a93790e384afd49d92e00ec1
Diana Bello <77254132+dianabello1@users.noreply.github.com>
2021-04-05 15:46:32 -0400

    Update Juegos_clases.py

--- Juegos_clases.py
+++ Juegos_clases.py
@@ -1,128 +1,61 @@
 import random   
 
-class Adivinanzas():
+class Mini_juegos():
     #Constructor 
-    def __self__(self, tiempo, vidas, pistas):
-        #Atributos 
-        self.tiempo = tiempo 
-        self.vidas = vidas
+    def __init__(self, respuestas, award, requerimiento, reglas, pistas): #Atributos 
+       
+        self.respuestas = respuestas
+        self.award = award
+        self.requerimiento = requerimiento
+        self.reglas = reglas
         self.pistas = pistas
     # Metodos de la clase 
-    def Adivinanzas_facil(self):
-        self.vidas = 5 
-        self.tiempo = 3
-        self.pistas = 3 
 
-    def Adivinanzas_medio(self):
-        self.vidas = 3
-        self.tiempo = 3
-        self.pistas = 2
-
-    def Adivinanzas_dificil(self):
-        self.vidas = 1
-        self.tiempo = 1 
-        self.pistas = 2  
+class Adivinanzas(Mini_juegos):
+    def __init__(self, respuestas, award, requerimiento, pistas, reglas, tablero):
+        Mini_juegos.__init__(self, respuestas, pistas, reglas, requerimiento, award)
+        self.tablero = tablero 
 
 class Ahorcado():
-    # Constructor 
-    def __self__(self, tiempo, vidas, pistas):
-        # Atributpos 
-        self.tiempo = tiempo 
-        self.vidas = vidas
-        self.pistas = pistas
-    # Metodos de clase
-    def Ahorcado_facil(self):
-        self.vidas = 5 
-        self.tiempo = 5 
-        self.pistas = 3 
-
-    def Ahorcado_medio(self):
-        self.vidas = 3 
-        self.tiempo = 3 
-        self.pistas = 3 
-
-    def Ahorcado_dificil(self):
-        self.vidas = 1 
-        self.tiempo = 2 
-        self.pistas = 3 
+    def __init__(self, respuestas, award, requerimiento, pistas, reglas, tablero):
+        pass
      
 class sopa_de_letras():
-    def __self__(self, tiempo, vidas, pistas):
-        self.tiempo = tiempo 
-        self.vidas = vidas
-        self.pistas = pistas
-
-    def sopa_de_letras_facil(self):
-        self.vidas = 5 
-        self.tiempo = 5 
-        self.pistas = 3 
-
-    def sopa_de_letras_medio(self):
-        self.vidas = 3 
-        self.tiempo = 3 
-        self.pistas = 3 
-
-    def sopa_de_letras_dificil(self):
-        self.vidas = 1 
-        self.tiempo = 1
-        self.pistas = 2 
+    def __init__(self, respuestas, award, requerimiento, pistas, reglas, tablero):
+        pass
 
 class Encuentra_la_logica():
-     def __self__(self, tiempo, vidas, pistas):
-        self.tiempo = tiempo 
-        self.vidas = vidas
-        self.pistas = pistas
-        
+    def __init__(self, respuestas, award, requerimiento, pistas, reglas, tablero):
+        pass
+
 class Preguntas_sobre_python():
-    def __self__(self, tiempo, vidas, pistas):
-        self.tiempo = tiempo 
-        self.vidas = vidas
-        self.pistas = pistas
+   def __init__(self, respuestas, award, requerimiento, pistas, reglas, tablero):
+       pass
 
 class Preguntas_de_matematicas():
-    def __self__(self, tiempo, vidas, pistas):
-        self.tiempo = tiempo 
-        self.vidas = vidas
-        self.pistas = pistas
+   def __init__(self, respuestas, award, requerimiento, pistas, reglas, tablero):
+       pass
 
 class Criptograma():
-    def __self__(self, tiempo, vidas, pistas):
-        self.tiempo = tiempo 
-        self.vidas = vidas
-        self.pistas = pistas
+    pass
 
 class Quizizz_cultura_unimetana():
-    def __self__(self, tiempo, vidas, pistas):
-        self.tiempo = tiempo 
-        self.vidas = vidas
-        self.pistas = pistas
+   pass
 
 class Memoria_con_emojis():
-    def __self__(self, tiempo, vidas, pistas):
-        self.tiempo = tiempo 
-        self.vidas = vidas
-        self.pistas = pistas   
+   def __init__(self, respuestas, award, requerimiento, pistas, reglas, tablero):
+       pass
 
 class Logica_buleana():
-    def __self__(self, tiempo, vidas, pistas):
-        self.tiempo = tiempo 
-        self.vidas = vidas
-        self.pistas = pistas
+    def __init__(self, respuestas, award, requerimiento, pistas, reglas, tablero):
+        pass
 
 class Palabra_mezcla():
-    def __self__(self, tiempo, vidas, pistas):
-        self.tiempo = tiempo 
-        self.vidas = vidas
-        self.pistas = pistas
+    def __init__(self, respuestas, award, requerimiento, pistas, reglas, tablero):
+        pass
 
 class Escoge_un_numero_entre():
-    def __self__(self, tiempo, vidas, pistas):
-        self.tiempo = tiempo 
-        self.vidas = vidas
-        self.pistas = pistas
+    def __init__(self, respuestas, award, requerimiento, pistas, reglas, tablero):
+        pass
 
 class Final_boss():
-    def __self__(self, tiempo, vidas, pistas):
-        self.tiempo = tiempo 
-        self.vidas = vidas
-        self.pistas = pistas
